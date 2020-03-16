81a7c3c33ee3f665a03892f1e513ee5855e3da41
evan <aspaasevan@gmail.com>
2020-03-16 12:12:06 -0700

    added game over button that resets scores

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
   const [lionsScore, setLionsScore] = useState(0);
   const [tigersScore, setTigersScore] = useState(0);
 
+  // where am i going with this - i want to reset both home/away score with one button (game over)
+  
+  const [gameScore, setGameScore] = useState(lionsScore + tigersScore);
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -29,13 +33,21 @@ function App() {
       </section>
       <section className="buttons">
         <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={() => setLionsScore(lionsScore + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => setLionsScore(lionsScore + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick={() => setTigersScore(tigersScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => setTigersScore(tigersScore + 3)}>Away Field Goal</button>
+        </div>
+
+        <div className="reset">
+          <button 
+            className="reset_button" 
+            onClick={function resetScores() {
+              setLionsScore(0);
+              setTigersScore(0);
+            }}>Game Over</button>
         </div>
       </section>
     </div>
