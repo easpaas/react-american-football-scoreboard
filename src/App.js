39c1b8819b1ef8827832dfa204faf837183af193
evan <aspaasevan@gmail.com>
2020-03-16 16:27:09 -0700

    passing props to buttons component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,26 @@
-//TODO: STEP 1 - Import the useState hook.
 import React, {useState} from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
+import Buttons from "./Buttons";
+import Scoreboard from "./Scoreboard";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [lionsScore, setLionsScore] = useState(0);
-  const [tigersScore, setTigersScore] = useState(0);
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
+
+  
 
   return (
     <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{lionsScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{tigersScore}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          <button className="homeButtons__touchdown" onClick={() => setLionsScore(lionsScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setLionsScore(lionsScore + 3)}>Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setTigersScore(tigersScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => setTigersScore(tigersScore + 3)}>Away Field Goal</button>
-        </div>
-
-        <div className="reset">
-          <button 
-            className="reset_button" 
-            onClick={function resetScores() {
-              setLionsScore(0);
-              setTigersScore(0);
-            }}>Game Over</button>
-        </div>
-      </section>
+      <Scoreboard 
+        homeScore={homeScore}
+        awayScore={awayScore} 
+      />
+      <Buttons 
+        homeScore={homeScore}
+        awayScore={awayScore} 
+        setHomeScore={setHomeScore}
+        setAwayScore={setAwayScore}
+      />
     </div>
   );
 }
